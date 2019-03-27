dd7effe89e8f92454e57426b13dfcc3c68beed70
502 <502@502-PC>
2019-03-27 16:49:12 +0900

    1

---
+++ HelloWorld18/src/main/java/itachi/uchiha/domain/MemberDTO.java
@@ -0,0 +1,12 @@
+package itachi.uchiha.domain;
+
+import java.io.Serializable;
+
+public class MemberDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
